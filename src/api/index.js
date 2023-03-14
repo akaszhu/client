998e840ce169d05b9340ad272482471f4007fa75
akaszhu <104959631+akaszhu@users.noreply.github.com>
2023-03-14 14:45:40 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000"
+    baseURL: "https://client-qhhs.onrender.com/"
   });
   API.interceptors.request.use((req) => {
     if (localStorage.getItem("Profile")) {
